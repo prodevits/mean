b98ac53ae52d7b0af3d9796f9da9c23380adaa7f
prodevits <prodev.its@gmail.com>
2020-05-18 12:09:02 +0530

    Structural directives demos

--- angws/myproject1/src/app/str-dirs/str-dirs.component.ts
+++ angws/myproject1/src/app/str-dirs/str-dirs.component.ts
@@ -6,15 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./str-dirs.component.css']
 })
 export class StrDirsComponent implements OnInit {
+// cities=['indore','mumbai','pune'];
 cities=[];
 cname:string;
 
+add(){
+  this.cities.push(this.cname);
+  this.cname=null;
+}
   constructor() { }
 
   ngOnInit() {
   }
 
-  addCity(){
-    this.cities.push(this.cname);
+  salary:number;
+  salaryList:number[]=[];
+
+  addSalary(){
+    this.salaryList.push(this.salary);
   }
+
+
 }
