df72ff016bb19f4db18505b0b630adfb6cf02828
prodevits <prodev.its@gmail.com>
2020-05-23 13:20:34 +0530

    ngStyle uses

---
+++ angws/myproject1/src/app/style-demo/style-demo.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-style-demo',
+  templateUrl: './style-demo.component.html',
+  styleUrls: ['./style-demo.component.css']
+})
+export class StyleDemoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  bgc = 'red';
+  gender='male';
+  country='india'
+
+
+
+  setBg(c) {
+    this.bgc = c;
+  }
+
+  setCountry(c) {
+    this.country = c;
+  }
+
+  getCountryColor(){
+    switch(this.country){
+      case 'india': return '#00f';
+      case 'usa': return '#f00';
+      case 'pak': return '#0f0';
+      case 'japan': return '#0ff';
+      default:return "#ff0";
+    }
+  }
+
+}
